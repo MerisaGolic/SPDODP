b401b7764dd1684bd1aa5350f2141be61b020750
Adi Palavra <apalavra1@etf.unsa.ba>
2017-03-27 18:42:43 +0200

    Konfiguracija za bazu

--- ModulZaOdredjivanjeTerapije/src/main/java/ba/unsa/etf/ModulZaOdredjivanjeTerapijeApplication.java
+++ ModulZaOdredjivanjeTerapije/src/main/java/ba/unsa/etf/ModulZaOdredjivanjeTerapijeApplication.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"ba.unsa.etf"})
@@ -11,4 +15,17 @@ public class ModulZaOdredjivanjeTerapijeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModulZaOdredjivanjeTerapijeApplication.class, args);
 	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
 }
