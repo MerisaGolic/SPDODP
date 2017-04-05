7e928d543db8ed91daa2885c977d9f4d91f3581c
Armin Nogo <anogo1@etf.unsa.ba>
2017-04-05 10:33:43 +0200

    Test komunikacije modula DijagnozePacijenti i Korisnici

--- DijagnozePacijenti/src/main/java/model/DijagnozePacijentiApplication.java
+++ DijagnozePacijenti/src/main/java/model/DijagnozePacijentiApplication.java
@@ -13,6 +13,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -36,4 +38,23 @@ class ServiceInstanceRestController {
             @PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
     }
+}
+
+@RestController
+@EnableFeignClients(basePackages = {"ba.unsa.etf", "md", "model"})
+class KomunikacijaController {
+	@Autowired DijagnozePacijentiFeign dpf;
+	
+	@RequestMapping("/get-pozdrav")
+    public String pozdrav(Model model) {
+        return "pozdrav";
+    }
+	
+	@RequestMapping(value= "/get-provjeri-pacijenta-test")
+	public Boolean provjeriPacijentaTest()
+	{
+		return dpf.provjeriPacijentaTest();
+	}
+	
+	
 }
